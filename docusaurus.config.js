24352045b6aaca86699da6dacb48dc02f1dec7cd
Vijay <vjwarboy13@gmail.com>
2024-01-06 09:50:43 +0530

    config updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://www.npmjs.com",
+  url: "https://github.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/princeofv/react-datatable-pro-docs",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
